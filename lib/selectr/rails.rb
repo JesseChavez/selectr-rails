8d5dda7902d132a7ca8291500064e5815d9fae6b
Jesse Chavez <jesse.chavez.r@gmail.com>
2018-06-20 20:47:49 +1000

    generate gem skeleton

---
+++ lib/selectr/rails.rb
@@ -0,0 +1,7 @@
+require "selectr/rails/version"
+
+module Selectr
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ spec/selectr/rails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Selectr::Rails do
+  it "has a version number" do
+    expect(Selectr::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
